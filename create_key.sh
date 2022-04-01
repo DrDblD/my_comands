a87c025f15f2336c1dd5cd3bb14a7a63518ebe8f
DrDblD <drdbld666@gmail.com>
2022-04-01 18:12:03 +0500

    add sshkey settings

--- create_key.sh
+++ create_key.sh
@@ -1,10 +1,18 @@
-DIRECTORY=~/.ssh
-if [ -d "$DIRECTORY" ]; then
-    echo "$DIRECTORY exists."
-else 
-    echo "$DIRECTORY does not exist."
-    mkdir 
-fi
-cd ~/.ssh && ssh-keygen
-ls | cat $(find . -name id_rsa*.pub) # | xclip # to copy to clipboard
-cd ~
+### Long sheet way ###
+# DIRECTORY=~/.ssh
+# if [ -d "$DIRECTORY" ]; then
+#     echo "$DIRECTORY exists."
+# else 
+#     echo "$DIRECTORY does not exist."
+#     mkdir $DIRECTORY
+# fi
+# cd ~/.ssh && ssh-keygen 
+# ls | cat $(find . -name id_rsa*.pub) # | xclip # to copy to clipboard
+# cd ~
+
+### one string way ###
+# echo "atab" | (read p1; ssh-keygen -f ~/.ssh/$p1; cat $(find . -name $p1.pub) )
+
+### two string way ###
+NAME=$@
+[ -n $NAME] && (ssh-keygen -f ~/.ssh/$NAME; cat $(find . -name $NAME.pub)) || (cd ~/.ssh && ssh-keygen && ls | cat $(find . -name id_rsa*.pub))
--- minicube_get.sh
+++ minicube_get.sh
@@ -1,4 +1,4 @@
 PACKAGE=minikube_latest_amd64.deb
 curl -LO https://storage.googleapis.com/minikube/releases/latest/$PACKAGE --output /tmp/$PACKAGE
-dpkg -i $PACKAGE
+dpkg -i /tmp/$PACKAGE
 rm /tmp/$PACKAGE
