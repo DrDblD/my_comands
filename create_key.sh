e4223c8427371e44465164876363f6c3e5b29e18
Andrey Taburkin <ataburkin@poidem.ru>
2022-04-11 15:40:24 +0500

    add hosts to local ssh config a

--- create_key.sh
+++ create_key.sh
@@ -11,8 +11,20 @@
 # cd ~
 
 ### one string way ###
-# echo "atab" | (read p1; ssh-keygen -f ~/.ssh/$p1; cat $(find . -name $p1.pub) )
+# echo "atab" | (read p1; ssh-keygen -f ~/.ssh/$p1; cat $(find . -name $p1.pub); read $host; echo -e "Host $host\n  HostName $host\n  User ${USER} \n  IdentityFile ~/.ssh/$p1\n  IdentitiesOnly yes\n  HostKeyAlgorithms=+ssh-rsa" >> ~/.ssh/config
 
 ### two string way ###
 NAME=$@
 [ -n $NAME] && (ssh-keygen -f ~/.ssh/$NAME; cat $(find . -name $NAME.pub)) || (cd ~/.ssh && ssh-keygen && ls | cat $(find . -name id_rsa*.pub))
+
+
+# for i in {1..4}; do
+#     host=webapps-test
+#     echo -e "Host $host$i\n  HostName $host$i\n  User ${USER} \n  IdentityFile ~/.ssh/${USER}}\n  IdentitiesOnly yes\n" >> ~/.ssh/config
+#     host=webapps-dev
+#     echo -e "Host $host$i\n  HostName $host$i\n  User ${USER} \n  IdentityFile ~/.ssh/${USER}}\n  IdentitiesOnly yes\n  HostKeyAlgorithms=+ssh-rsa" >> ~/.ssh/config
+# done
+
+# Host github.com
+#   HostName github.com
+#   IdentityFile ~/.ssh/github
