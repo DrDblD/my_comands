b30ab3db8f60a17bfc66aa4afefb7825dfd6f80a
Andrey Taburkin <ataburkin@poidem.ru>
2022-05-23 16:18:54 +0500

    add some features

---
+++ kubernetes/argoCD.sh
@@ -0,0 +1,14 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+argocd login <ARGOCD_SERVER>
+argocd account update-password
+kubectl config get-contexts -o name
+argocd cluster add current context
+argocd app get guestbook 
+
+#@# remove
+kubectl get all,cm,ingress,job,secret -n argocd -o name | xargs kubectl delete -n argocd
+kubectl delete namespaces argocd
---
+++ kubernetes/kapitan.sh
@@ -0,0 +1,19 @@
+if test -d /tmp/la/;
+then
+    rm -rf /tmp/la/*;
+else
+    mkdir /tmp/la;
+    ln -sP /tmp/la ~/builds;
+fi
+STPATH=${PWD}
+KAPATH=/tmp/la
+cp -r ~/repos/LISA/lisa-kube/* $KAPATH/
+KAPITAN_IMAGE=kapicorp/kapitan:v0.30.0-rc.0
+
+# cd $KAPATH && \
+# py yaml_concat.py lisa-dev && \
+# find $KAPATH/inventory/targets/ -maxdepth 1 -name lisa-* -type d -exec rm -rf {} \;
+# cd $STPATH && \
+docker run --rm -i -w /src \
+   -v $KAPATH/:/src:Z \
+   $KAPITAN_IMAGE $@
