09390893849d8d82ad5908e1e874a3e865879614
Andrey Taburkin <ataburkin@poidem.ru>
2022-06-09 18:46:37 +0500

    play with jira

--- jira/atlassian-agent/create.sh
+++ jira/atlassian-agent/create.sh
@@ -1,1 +1,3 @@
-docker run -it --rm --entrypoint /bin/bash --name my-maven-project -v "$(pwd)":/usr/src/mymaven:z -w /usr/src/mymaven maven:3.3-jdk-8 maven package
+# site https://gitee.com/pengzhile/atlassian-agent
+# docker run -it --rm --entrypoint /bin/bash --name my-maven-project -v "$(pwd)":/usr/src/mymaven:z -w /usr/src/mymaven maven:3.3-jdk-8
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven:z -w /usr/src/mymaven maven:3.3-jdk-8 mvn package
